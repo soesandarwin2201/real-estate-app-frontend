5b0fec7f19bcd80299e186b7b018a45026cc922f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-07-10 15:33:09 +0630

    create the footer navbar styling

--- src/layouts/navbar/navData.js
+++ src/layouts/navbar/navData.js
@@ -1,3 +1,7 @@
+import * as BiMap from 'react-icons/bi';
+import * as FiHome from 'react-icons/fi';
+import * as BsPerson from 'react-icons/bs';
+
 export const navLinks = [
      {
        id: "home",
@@ -20,3 +24,65 @@ export const navLinks = [
        title: "Agents",
      },
    ];
+
+export const mobile_nav = [
+  {
+    id: "all",
+    path: '/all',
+    title: "All",
+  },
+  {
+    id: "buy",
+    path: '/buy',
+    title: "Buy",
+  },
+  {
+    id: "rent",
+    path: '/rent',
+    title: "Rent",
+  },
+  
+]
+
+export const footer_nav = [
+  {
+    id: "home",
+    path: '/home',
+    title: "Home",
+    icon: <FiHome.FiHome />,
+    className: 'footer_nav_item',
+    linkcss: 'footer_nav_link'
+  },
+  {
+    id: "agents",
+    path: '/agents',
+    title: "Agents",
+    icon: <BsPerson.BsPeople />,
+    className: 'footer_nav_item',
+    linkcss: 'footer_nav_link'
+  },
+  {
+    id: "sell",
+    path: '/sell',
+    title: "Sell",
+    icon: <FiHome.FiPlus  />,
+    className: '',
+    linkcss: 'plus'
+  },
+  {
+    id: "chat",
+    path: '/chat',
+    title: "Chat",
+    icon: <BsPerson.BsChatDots  />,
+    className: 'footer_nav_item',
+    linkcss: 'footer_nav_link'
+  },
+  {
+    id: "signup",
+    path: '/signup',
+    title: "Sign Up",
+    icon: <BsPerson.BsPerson  />,
+    className: 'footer_nav_item',
+    linkcss: 'footer_nav_link'
+  },
+]
--- src/layouts/navbar/navbar.jsx
+++ src/layouts/navbar/navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from "react-router-dom";
 import { Link} from 'react-router-dom';
-import { BiMap } from 'react-icons/bi'
-import { navLinks } from './navData';
+import { BiMap } from 'react-icons/bi';
+import { navLinks, mobile_nav, footer_nav} from './navData';
 import './nav.css'
 
 const Navbar = () => {
      const location = useLocation();
+     const [ active, setActive ] = useState("");
    
      if (location.pathname === "/") {
        return null;
@@ -14,17 +15,41 @@ const Navbar = () => {
    
      return (
        <nav className="navbar">
-         <Link to="/home" className="nav-logo">
+         <Link to="/home" className="nav-logo desktop">
           Mandalar May <BiMap  className='map-icon' />
          </Link>
-         <ul className="nav-menu">
+         <form action="" className="mobile">
+          <input type="text" placeholder='Search your dream house'  className='search_input' />
+         </form>
+         <ul className="nav-menu desktop">
           { navLinks.map((link) => (
-               <li className="nav-items" key={link.id}>
+               <li className={ `${ active === link.title ? "active" : "nav-items"}`} key={link.id} onClick={() => setActive(link.title)}>
                     <Link to={link.path} className="nav-link">{link.title}</Link>
                </li>
           ))}
           <button className="signup-btn">SignUp</button>
          </ul>
+         
+         <ul className="mobile-nav-menu mobile">
+          { mobile_nav.map((link) => (
+               <li className={ `${ active === link.title ? "active" : "nav-items"}`} key={link.id} onClick={() => setActive(link.title)}>
+                    <Link to={link.path} className="nav-link">{link.title}</Link>
+               </li>
+          ))}
+         </ul>
+
+         <ul className="footer_nav mobile">
+         {
+        footer_nav.map((item) => (
+          <li className={item.className} key={item.id}>
+            <Link to={item.path} className={item.linkcss}>
+              {item.icon}
+            </Link>
+          </li>
+        ))
+      }
+         </ul>
+
        </nav>
      );
    };
